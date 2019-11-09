aae570ec5cc20c06c9e0b4375def689634167b25
jecinta <jecintawanjirug@gmail.com>
2019-11-09 10:35:17 +0300

    this creates a function that gets data using get_category

--- photos/views.py
+++ photos/views.py
@@ -16,11 +16,25 @@ def search_results(request):
         searched_image = Image.search_by_category(search_term)
         message = f"{search_term}"
 
-        return render(request, 'welcome.html',{"message":message,"all_images": searched_image})
+        return render(request, 'search.html',{"message":message,"all_images": searched_image})
 
     else:
-        message = "You haven't searched for any term"
-        return render(request, 'welcome.html',{"message":message})
+        message = "You haven't searched for any category"
+        return render(request, 'search.html',{"message":message})
+
+
+def get_category(request,category):
+    category_results = Category.objects.all()
+    location_results = Location.objects.all()
+    category_result = Image.objects.filter(image_category__category_name = category)
+    return render(request,'welcome.html',{'all_images':category_result,'category_results':category_results,'location_results':location_results})
+
+def get_location(request,location):
+    category_results = Category.objects.all()
+    location_results = Location.objects.all()
+    location_result = Image.objects.filter(image_location__location_name= location)
+    return render(request,'welcome.html',{'all_images':location_result,'category_results':category_results,'location_results':location_results})
+
 
 
 
