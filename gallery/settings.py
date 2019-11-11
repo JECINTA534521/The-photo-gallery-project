dc068ae0e222401095a0870490a384e4a9bf1318
jecinta <jecintawanjirug@gmail.com>
2019-11-11 12:38:23 +0300

    change settings.py

--- gallery/settings.py
+++ gallery/settings.py
@@ -101,9 +101,9 @@ DATABASES = {
 #        )
 #    }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+    
 ALLOWED_HOSTS = []
 
 
